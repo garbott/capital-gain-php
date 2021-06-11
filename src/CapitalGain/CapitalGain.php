ca624062a70ac146452bce9d2abd4e78c665945e
Joseph Garbett <joseph@garbott.co.uk>
2021-06-12 00:26:35 +0100

    First calculator commit,

---
+++ src/CapitalGain/CapitalGain.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CapitalGain;
+
+class CapitalGain
+{
+  
+    /**
+     * @returns string
+     */
+    public static function calculator()
+    {
+        return 'Hello World, Composer!';
+    }
+}
